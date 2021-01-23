b490b780fe4faaadecc46dedfb82c48e79bd6902
Gabriel Denis López <gabrieldenislopez@gmail.com>
2021-01-23 19:08:07 -0300

    Modal & Button Styling

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [modalDisplay, setModalDisplay] = useState(false);
 
-  const addGoalHandler = (goalTitle, clearInput) => {
+  const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle },
     ]);
-    clearInput('');
+    setModalDisplay(false);
   };
 
   const deleteGoalHandler = goalId => {
@@ -21,9 +22,24 @@ export default function App() {
     );
   };
 
+  const cancelGoalAddition = () => {
+    setModalDisplay(false);
+  };
+
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <View style={styles.buttonContainer}>
+        <Button
+          color="white"
+          title="Add new Goal!"
+          onPress={() => setModalDisplay(true)}
+        />
+      </View>
+      <GoalInput
+        modalDisplay={modalDisplay}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAddition}
+      />
       <FlatList
         keyExtractor={(item, key) => item.id} // another way to tell how to set each key, if the array of objects doesn't have a key prop
         data={courseGoals}
@@ -42,5 +58,10 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 60,
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: 'blue',
   },
 });
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,33 +1,52 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Modal,
+  TouchableOpacity,
+} from 'react-native';
 
-const GoalInput = ({ onAddGoal }) => {
+const GoalInput = ({ onAddGoal, onCancel, modalDisplay }) => {
   const [enteredGoal, setEnteredGoal] = useState('');
 
   const goalInputHandler = enteredText => {
     setEnteredGoal(enteredText);
   };
 
+  const onAddGoalHandler = () => {
+    onAddGoal(enteredGoal);
+    setEnteredGoal('');
+  };
+
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Course Goal"
-        style={styles.input}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
-      />
-      <Button
-        title="Add"
-        onPress={() => onAddGoal(enteredGoal, goalInputHandler)}
-      />
-    </View>
+    <Modal visible={modalDisplay} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="New Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.buttonAdd}>
+            <Button title="Add" color="white" onPress={onAddGoalHandler} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonCancel}>
+            <Button title="Cancel" color="white" onPress={onCancel} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
   },
   input: {
@@ -35,6 +54,22 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     width: '80%',
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    width: '80%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  buttonAdd: {
+    width: '40%',
+    borderRadius: 10,
+    backgroundColor: 'blue',
+  },
+  buttonCancel: {
+    width: '40%',
+    borderRadius: 10,
+    backgroundColor: 'red',
   },
 });
 
